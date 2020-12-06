9b69838039fad359b6bd7960e40771e2684b5ec6
stacksideflow <1@stacksideflow.host>
2020-12-06 18:37:10 +0300

    [WIP] Link/rename hegex NFT contracts

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,13 +1,37 @@
-var Migrations = artifacts.require("Migrations.sol");
-const {env, smartContractsPath} = require("../truffle.js");
-const {writeSmartContracts,getSmartContractAddress, readSmartContractsFile, setSmartContractAddress} = require("./utils.js");
+const { env, smartContractsPath } = require("../truffle.js");
+const { writeSmartContracts, getSmartContractAddress, readSmartContractsFile, setSmartContractAddress } = require("./utils.js");
 
-module.exports = async function(deployer, network, accounts) {
-  await deployer.deploy(Migrations, {gas: 1000000, from: accounts[0]});
 
-  var smartContracts = readSmartContractsFile(smartContractsPath);
+let smartContractsList = [];
+
+const Migrations = artifacts.require("Migrations");
+const chef = artifacts.require('OptionChef');
+const token = artifacts.require('Hegexoption.sol');
+// Swap for real Hegic option factory address
+const HEGIC_ADDRESS = "0xefc0eeadc1132a12c9487d800112693bf49ecfa2";
+const METADATA_BASE = "https://stacksideflow.github.io/hegexoption-nft/meta/"
+
+module.exports = async (deployer) => {
+  await deployer.deploy(Migrations);
   const migrations = await Migrations.deployed();
+  await deployer.deploy(chef, HEGIC_ADDRESS);
+  const chefd = await chef.deployed();
+  await deployer.deploy(token, chefd.address, METADATA_BASE);
+  const tokend = await token.deployed();
 
+  let smartContracts = readSmartContractsFile(smartContractsPath);
+  // two lines below are redundant
   setSmartContractAddress(smartContracts, ":migrations", migrations.address);
   writeSmartContracts(smartContractsPath, smartContracts, env);
+
+  assignContract(chefd, "OptionChef", "optionchef");
+  assignContract(tokend, "Hegexoption", "hegexoption");
+  writeSmartContracts(smartContractsPath, smartContractsList, env)
 };
+
+function assignContract(contract_instance, contractName, contract_key, opts) {
+  console.log("- Assigning '" + contractName + "' to smart contract listing...");
+  opts = opts || {};
+  smartContractsList = smartContractsList.concat(
+    encodeContractEDN(contract_instance, contractName, contract_key, opts));
+}
