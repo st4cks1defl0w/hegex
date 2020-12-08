b0c612c36cf47c143a21884bea4c8bc1f039d5f4
stacksideflow <1@stacksideflow.host>
2020-12-08 11:43:05 +0300

    [DEV] Rm cljs-0x-connect from deps

--- project.clj
+++ project.clj
@@ -70,7 +70,7 @@
                  [print-foo-cljs "2.0.3"]
                  [re-frame "0.10.5"]
                  [reagent "0.8.1"]
-                 [district0x/cljs-0x-connect "1.0.0"]]
+                 #_[district0x/cljs-0x-connect "1.0.0"]]
 
   :exclusions [express-graphql
                cljsjs/react-with-addons
