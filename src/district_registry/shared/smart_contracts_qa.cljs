33440de463459c74fdcb6e4a9c6491f4fc6b61a5
stacksideflow <1@stacksideflow.host>
2020-12-10 00:51:49 +0300

    Add Hegic ETH options ropsten contract to the frontend

--- src/district_registry/shared/smart_contracts_qa.cljs
+++ src/district_registry/shared/smart_contracts_qa.cljs
@@ -1,4 +1,6 @@
 (ns district-registry.shared.smart-contracts-qa)
 
 (def smart-contracts
-  {:optionchef {:name "OptionChef" :address "0x5d8c37fa1cc6b7de5980c8b66f3e7e714cb3dc31"} :hegexoption {:name "Hegexoption" :address "0x1f12132e83bec1431221023f85c964305df535d7"}})
+  {:optionchef {:name "OptionChef" :address "0x5d8c37fa1cc6b7de5980c8b66f3e7e714cb3dc31"}
+   :hegexoption {:name "Hegexoption" :address "0x1f12132e83bec1431221023f85c964305df535d7"}
+   :brokenethoptions {:name "BrokenETHOptions" :address "0x77041D13e0B9587e0062239d083b51cB6d81404D"} })
