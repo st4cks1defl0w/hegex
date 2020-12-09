40faace92e1010de375739e0ad8321187ac55ede
stacksideflow <1@stacksideflow.host>
2020-12-10 00:27:28 +0300

    Branch out Hegic ETH factory addr by netID

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -5,26 +5,29 @@ const { encodeContractEDN, writeSmartContracts, getSmartContractAddress, readSma
 
 let smartContractsList = [];
 
+const hegicETHFactory = {
+  "ropsten": "0x77041D13e0B9587e0062239d083b51cB6d81404D",
+  "mainnet": "0xefc0eeadc1132a12c9487d800112693bf49ecfa2",
+}
+
+return smartContractsPaths[netToEnv[netID] || "dev"];
+
 const Migrations = artifacts.require("Migrations");
 const chef = artifacts.require('OptionChef');
 const token = artifacts.require('Hegexoption.sol');
-// Swap for real Hegic option factory address
-const HEGIC_ADDRESS = "0xefc0eeadc1132a12c9487d800112693bf49ecfa2";
 const METADATA_BASE = "https://stacksideflow.github.io/hegexoption-nft/meta/"
 
 module.exports = async (deployer, network) => {
   console.log("Migrating Hegex to " + network);
   await deployer.deploy(Migrations);
   const migrations = await Migrations.deployed();
-  await deployer.deploy(chef, HEGIC_ADDRESS);
+  //important - will throw on localnet as hegic contracts are deployed separately
+  await deployer.deploy(chef, hegicETHFactory[network]);
   const chefd = await chef.deployed();
   await deployer.deploy(token, chefd.address, METADATA_BASE);
   const tokend = await token.deployed();
 
   let smartContracts = readSmartContractsFile(smartContractsPath);
-  // two lines below are redundant
-  // setSmartContractAddress(smartContracts, ":migrations", migrations.address);
-  // writeSmartContracts(smartContractsPath, smartContracts, env);
 
   assignContract(chefd, "OptionChef", "optionchef");
   assignContract(tokend, "Hegexoption", "hegexoption");
