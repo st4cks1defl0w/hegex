be66bf200bf1834f1aed7837197d571acb01f4c8
stacksideflow <1@stacksideflow.host>
2020-12-25 17:53:50 +0300

    [DEV] Add squashed 0x commits

--- src/district_registry/ui/trading/events.cljs
+++ src/district_registry/ui/trading/events.cljs
@@ -16,3 +16,22 @@
   interceptors
   (fn [{:keys [db]} [id]]
     (println "dbg WIP creating 0x offer for id" id)))
+
+
+
+(def ^:private token-pair
+  {:base-token-address "0xe41d2489571d322189246dafa5ebde1f4699f498"
+   :quote-token-address "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"})
+
+(defn- orders []
+  (js-invoke
+   (http-client/get-orderbook-async
+          apiclient
+          {:request token-pair}) "then"
+         (fn [r]
+           (println "orders are" r))))
+
+;;TODO resolve CORS problem, outdated api endpoint?
+
+;;NOTE repl functions
+#_(orders)
