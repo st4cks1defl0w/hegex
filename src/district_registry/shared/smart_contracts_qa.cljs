036caf2bf7e83e08046d6f3aef71514191bf9b8c
stacksideflow <1@stacksideflow.host>
2020-12-25 14:01:46 +0300

    Update testnet addresses

--- src/district_registry/shared/smart_contracts_qa.cljs
+++ src/district_registry/shared/smart_contracts_qa.cljs
@@ -1,4 +1,6 @@
-(ns district-registry.shared.smart-contracts-dev)
+(ns district-registry.shared.smart-contracts-qa)
 
 (def smart-contracts
-  {:optionchef {:name "OptionChef" :address "0x9877097f81835da08dc64296b3bff7ea530de459"} :hegexoption {:name "Hegexoption" :address "0x16608d5e089d27b6e7b698b843a5c3356ad1b90a"}})
+  {:optionchef {:name "OptionChef" :address "0x6bf9f2c5c2ad1bddea6ff432a4f58ed2e3ef562a"}
+   :hegexoption {:name "Hegexoption" :address "0x3ea0eab5fc002c0b02842996cbed4ce2e20ee7c5"}
+   :brokenethoptions {:name "BrokenETHOptions" :address "0x77041D13e0B9587e0062239d083b51cB6d81404D"}})
